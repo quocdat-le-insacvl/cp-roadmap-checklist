a515b4a2a7e66b67c8eea1d6275d41cf27dbd715
LE Quoc Dat <quocdat.le.insacvl@gmail.com>
2021-10-05 23:08:44 +0200

    finished crawling data, prepare csv library

---
+++ cmake-build-debug/CMakeFiles/generate_md_file.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/generate_md_file.dir/generate_md_file.cpp.obj"
+  "generate_md_file.exe"
+  "generate_md_file.exe.manifest"
+  "generate_md_file.pdb"
+  "libgenerate_md_file.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/generate_md_file.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
